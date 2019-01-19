5d717349032a1857edd81bded80a2ebd51516bd0
visualstudio <visualstudio@122-PC04>
2019-01-18 20:43:43 -0500

    my next step

--- C0732104/Program.cs
+++ C0732104/Program.cs
@@ -10,6 +10,26 @@ namespace C0732104
     {
         static void Main(string[] args)
         {
+            StudentInformation _Mary = new StudentInformation("Mary", 3.8, 20);
+            StudentInformation _John = new StudentInformation("John", 2.0, 10);
+            StudentInformation _Mark = new StudentInformation("Mark", 0.2, 3);
+            StudentInformation _Steve = new StudentInformation("Steve", 2.9, 16);
+            StudentInformation _Cindy = new StudentInformation("Cindy", 3.9, 187);
+            
+        }
+    }
+
+
+    class StudentInformation
+    {
+        public string Name;
+        public double GPA;
+        public int Attendance;
+        public StudentInformation(string aName, double cGPA, int cAttendance)
+        {
+            Name = aName;
+            GPA = cGPA;
+            Attendance = cAttendance;
         }
     }
 }
